f8ebd4a8dda3ce307d719f6ee3f4a3a0d7245d2d
¨Fabiola¨ <ruiz.rios.fabiola05@gmail.com>
2019-01-17 12:36:31 -0600

    Función decode aprobada en TEST y funcionando en Navegador

--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ let offsetRecibido = document.getElementById("desplazamiento").value;
 document.getElementById("mensajefinal").innerHTML=window.cipher.encode(offsetRecibido,palabraRecibida);
 
 }); 
+
+
+document.getElementById("decodificar").addEventListener("click", () =>{
+   let palabraOtorgada= document.getElementById("mensaje").value;
+   let offsetOtorgado= document.getElementById("desplazamiento").value;
+
+document.getElementById("mensajefinal").innerHTML=window.cipher.decode(offsetOtorgado,palabraOtorgada);
+
+} );
