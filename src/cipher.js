93c00acfb40b051c25953bfa436d72a8b411330a
¨Fabiola¨ <ruiz.rios.fabiola05@gmail.com>
2019-01-21 13:04:34 -0600

    Java Script comentado

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,30 @@
 window.cipher = {
 
 
-
+//función codificar
   encode: (offsetRecibido,palabraRecibida) => {
-    let i=0;
-    palabraRecibida= palabraRecibida.toUpperCase();
-    let nuevaPalabra="";
-    for(i=0; i<palabraRecibida.length; i++){
+    let i=0; //Declaramos la variable "i"
+    palabraRecibida= palabraRecibida.toUpperCase(); //Aseguramos que la palabra este en mayúscula
+    let nuevaPalabra=""; //Creamos una variable que guarde "los resultados" de la función
+    for(i=0; i<palabraRecibida.length; i++){ //Iniciamos control de flujo
       let palabraAscii= (palabraRecibida.charCodeAt(i) - 65 + parseInt(offsetRecibido)) %26 +65;
       let palabraNormal= String.fromCharCode(palabraAscii);
       nuevaPalabra+=palabraNormal;
      }
-     return nuevaPalabra;
+     return nuevaPalabra; //Regresa la nueva palabra (el resultado)
   },
 
+  //función decodificar
   decode: (offsetOtorgado,palabraOtorgada) => {
-    let i=0;
-    palabraOtorgada=palabraOtorgada.toUpperCase();
-    let newWord="";
-    for(i=0; i<palabraOtorgada.length; i++){
+    let i=0;//Declaramos la variable "i"
+    palabraOtorgada=palabraOtorgada.toUpperCase();//Aseguramos que la palabra este en mayúscula
+    let newWord=""; //Creamos una variable que guarde "los resultados" de la función
+    for(i=0; i<palabraOtorgada.length; i++){ //Iniciamos control de flujo
       let asciiPalabra= (palabraOtorgada.charCodeAt(i) + 65 - parseInt(offsetOtorgado)) %26 + 65;
       let normalPalabra= String.fromCharCode(asciiPalabra);
       newWord+=normalPalabra;
     }
-    return newWord;
+    return newWord; //Regresa la nueva palabra (el resultado)
   },
 
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 
+
+
+//Evento del botón codificar
+// Llama al elemento por su "id" identificador, y ejecutara las funciones otorgadas al dar click
 document.getElementById("codificar").addEventListener("click",()=>{
-
+//Obtiene el contenido otorgado por el usuario (en este caso el mensaje a codificar)
 let palabraRecibida= document.getElementById("mensaje").value;
-
+//Obtiene el contenido otorgado por el usuario (el número de desplazamiento)
 let offsetRecibido = document.getElementById("desplazamiento").value;
 
-//window.cipher.encode(offsetRecibido,palabraRecibida);
-
+//Se posiciona en el elemento por su "id" identificador y muestra el resultado del la función "encode"
 document.getElementById("mensajefinal").innerHTML=window.cipher.encode(offsetRecibido,palabraRecibida);
 
 }); 
 
 
+
+//Evento del botón decodificar
+//Llama al elemento por su "id" identificador, y ejecutara las funciones otorgadas al dar click
 document.getElementById("decodificar").addEventListener("click", () =>{
+ //Obtiene el contenido otorgado por el usuario (en este caso el mensaje a codificar)  
    let palabraOtorgada= document.getElementById("mensaje").value;
+ //Obtiene el contenido otorgado por el usuario (el número de desplazamiento)  
    let offsetOtorgado= document.getElementById("desplazamiento").value;
 
+//Se posiciona en el elemento por su "id" identificador y muestra el resultado del la función "decode"
 document.getElementById("mensajefinal").innerHTML=window.cipher.decode(offsetOtorgado,palabraOtorgada);
 
 } );
